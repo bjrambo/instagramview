84e1a5d9fcd51c2a8ee4f9db4658c398574ca0d7
BJRambo <sosifam@070805.co.kr>
2018-09-28 22:11:46 +0900

    Code cleanup. and add to comment.

--- instagramView/InstagramClient.cs
+++ instagramView/InstagramClient.cs
@@ -37,7 +37,9 @@ namespace instagramView
 
         }
 
-
+        /**
+         * Get to code: http://kimstar.kr/2452/
+         */
         public static object GetMiddleString(string str, string begin, string end)
         {
             if (string.IsNullOrEmpty(str))
@@ -49,8 +51,14 @@ namespace instagramView
             if (str.IndexOf(begin) > -1)
             {
                 str = str.Substring(str.IndexOf(begin) + begin.Length);
-                if (str.IndexOf(end) > -1) result = str.Substring(0, str.IndexOf(end));
-                else result = str;
+                if (str.IndexOf(end) > -1)
+                {
+                    result = str.Substring(0, str.IndexOf(end));
+                }
+                else
+                {
+                    result = str;
+                }
             }
             return JsonConvert.DeserializeObject(result);
         }
