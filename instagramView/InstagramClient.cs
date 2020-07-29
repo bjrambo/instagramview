ba5baa0fa4fc6a9768dc8e93bb4435ec4adadda2
BJRambo <bjrambo@users.noreply.github.com>
2020-07-29 11:33:17 +0900

    Delete not use method

--- instagramView/InstagramClient.cs
+++ instagramView/InstagramClient.cs
@@ -78,31 +78,5 @@ namespace instagramView
                 return false;
             }
         }
-
-        /**
-         * Get to code: http://kimstar.kr/2452/
-         */
-        public static string GetMiddleString(string str, string begin, string end)
-        {
-            if (string.IsNullOrEmpty(str))
-            {
-                return null;
-            }
-
-            string result = null;
-            if (str.IndexOf(begin) > -1)
-            {
-                str = str.Substring(str.IndexOf(begin) + begin.Length);
-                if (str.IndexOf(end) > -1)
-                {
-                    result = str.Substring(0, str.IndexOf(end));
-                }
-                else
-                {
-                    result = str;
-                }
-            }
-            return result;
-        }
     }
 }
